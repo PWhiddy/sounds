81b312f4956b170ac359d3df361a6b43dee55f16
pdubs <all.cows.like.to.moo@gmail.com>
2018-06-23 20:07:36 -0700

    cleaned up

---
+++ make_sound.sh
@@ -0,0 +1,3 @@
+stack build
+stack exec music
+play test.wav
--- src/Main.hs
+++ src/Main.hs
@@ -4,19 +4,28 @@ import Data.WAVE
 num_channels = 1
 sample_rate :: Int
 sample_rate = 24000
+
 inv_sample_rate :: Double
 inv_sample_rate = 1.0 / (fromInteger (toInteger sample_rate) :: Double)
 bits_per_sample = 32
+sound_duration = 8
+sample_count = sound_duration * sample_rate
+base_freq = 650
+tau = 2*pi
 
 header = WAVEHeader num_channels sample_rate bits_per_sample Nothing
 
+sample_times :: [Double]
+sample_times = take sample_count [0, inv_sample_rate..]
+
 samples :: WAVESamples
-samples = fmap ((:[]) . doubleToSample . sin . (*freq)) [1..260000]
-  where freq = inv_sample_rate*1200
+samples = fmap ((:[]) . doubleToSample . wave) sample_times
+
+wave :: Double -> Double
+wave t = sin ( tb + 400 * sin ( 0.003 * tb  ) )
+  where tb = t * base_freq * tau
 
 main :: IO ()
 main = do
-  --print samples
   putWAVEFile "test.wav" (WAVE header samples)
   --putStrLn $ show $ fmap (show . doubleToSample) [0.1,0.2..1.0]
---}
